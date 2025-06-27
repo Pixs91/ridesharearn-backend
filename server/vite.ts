8a5d766b749f039b0271c15c8c2412def244cda5
Pixs91 <149877858+Pixs91@users.noreply.github.com>
2025-06-27 13:24:26 +0300

    Add files via upload

---
+++ server/vite.ts
@@ -0,0 +1,63 @@
+import express, { type Express } from "express";
+import fs from "fs";
+import path from "path";
+import { createServer as createViteServer, createLogger } from "vite";
+import { type Server } from "http";
+import { fileURLToPath } from "url";
+import { nanoid } from "nanoid";
+
+// ESM workaround for __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// ? Make sure vite.config.ts is compiled to vite.config.js
+import viteConfig from "../vite.config.js";
+
+const viteLogger = createLogger();
+
+export function log(message: string, source = "express") {
+  const formattedTime = new Date().toLocaleTimeString("en-US", {
+    hour: "numeric",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  });
+
+  console.log(`${formattedTime} [${source}] ${message}`);
+}
+
+export async function setupVite(app: Express, server: Server) {
+  const serverOptions = {
+    middlewareMode: true,
+    hmr: { server },
+    allowedHosts: true,
+  };
+
+  const vite = await createViteServer({
+    ...viteConfig,
+    configFile: false,
+    customLogger: {
+      ...viteLogger,
+      error: (msg, options) => {
+        viteLogger.error(msg, options);
+        process.exit(1);
+      },
+    },
+    server: serverOptions,
+  });
+
+  app.use(vite.middlewares);
+}
+
+export function serveStatic(app: Express) {
+  const distPath = path.resolve(__dirname, "../dist/public");
+
+  if (!fs.existsSync(distPath)) {
+    throw new Error(`Dist folder not found: ${distPath}`);
+  }
+
+  app.use(express.static(distPath));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(distPath, "index.html"));
+  });
+}
